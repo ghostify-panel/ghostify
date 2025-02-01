8b8bebefd6e46333727a4ab6afabb1be322d7ba5
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2025-02-02 07:52:14 +1100

    feat: flake and git ignore

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem = {
+        lib,
+        pkgs,
+        system,
+        config,
+        ...
+      }: 
+      {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (import inputs.rust-overlay)
+          ];
+        };
+
+        devShells.default = with pkgs; let 
+				toolchain = break ((pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
+          extensions = [ "rust-analyzer" "rust-src" ];
+          targets = [ "wasm32-unknown-unknown" ];
+        });
+
+        in mkShell
+        {
+          nativeBuildInputs = with pkgs; [
+            openssl
+						toolchain
+          ] ++ lib.optionals stdenv.isDarwin [
+						apple-sdk
+          ];
+        };
+      };
+    };
+}
